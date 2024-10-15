d6950ba81d1cb751a2a385cea8ea72c92cce347f
MarinaPaley <marina_paley@mail.ru>
2024-10-15 18:36:33 +0300

    Добавьте файлы проекта.

---
+++ TreeTests/TreeTests.cpp
@@ -0,0 +1,16 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TreeTests
+{
+	TEST_CLASS(TreeTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
